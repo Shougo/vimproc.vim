fb4969f103969a6fe4ffadd971b74271c383ea7c
Shougo.Matsu@gmail.com <Shougo.Matsu@gmail.com>
2009-06-29 14:53:43 +0900

    Use openpty();

--- autoload/proc.c
+++ autoload/proc.c
@@ -447,11 +447,13 @@ vp_pty_open(char *args)
     vp_stack_t stack;
     int argc;
     char *argv[VP_ARGC_MAX];
-    int fdm;
+    /*int fdm;*/
     pid_t pid;
     struct winsize ws = {0, 0, 0, 0};
     struct termios ti;
     int i;
+
+    int master, slave;
 
     VP_RETURN_IF_FAIL(vp_stack_from_args(&stack, args));
     VP_RETURN_IF_FAIL(vp_stack_pop_num(&stack, "%hu", &(ws.ws_col)));
@@ -471,13 +473,31 @@ vp_pty_open(char *args)
                 strerror(errno));
     pid = forkpty(&fdm, NULL, &ti, &ws);
 #else
-    pid = forkpty(&fdm, NULL, NULL, &ws);
+    /*pid = forkpty(&fdm, NULL, NULL, &ws);*/
+
+    /* Get current termios. */
+    tcgetattr(0, &ti);
+
+    /* Get current winsize. */
+    ioctl(0, TIOCGWINSZ, (char*)&ws);
+
+    /* Open pty. */
+    if (openpty(&master, &slave, NULL, &ti, &ws) == -1) {
+        return vp_stack_return_error(&_result, "openpty() error: %s",
+                strerror(errno));
+    }
+
+    pid_t pid = fork();
 #endif
     if (pid < 0) {
         return vp_stack_return_error(&_result, "forkpty() error: %s",
                 strerror(errno));
     } else if (pid == 0) {
         /* child */
+        close(master);
+        /* Create file descryptor. */
+        dup2(slave, 0); dup2(slave, 1); dup2(slave, 2);
+        close(slave);
         if (execv(argv[0], argv) < 0) {
             /* error */
             write(fdm, strerror(errno), strlen(strerror(errno)));
@@ -485,9 +505,10 @@ vp_pty_open(char *args)
         }
     } else {
         /* parent */
+        close(slave);
         vp_stack_push_num(&_result, "%d", pid);
-        vp_stack_push_num(&_result, "%d", fdm);
-        vp_stack_push_str(&_result, ttyname(fdm));
+        vp_stack_push_num(&_result, "%d", master);
+        vp_stack_push_str(&_result, ttyname(master));
         return vp_stack_return(&_result);
     }
     /* DO NOT REACH HEAR */
