9e7e5fa629ab7a20c4b42be9f29f408da4b4f7af
K.Takata <kentkt@csc.jp>
2015-10-06 22:53:18 +0900

    appveyor: Remove unused packages from MSYS2

--- tools/appveyor.bat
+++ tools/appveyor.bat
@@ -69,7 +69,7 @@ set CHERE_INVOKING=yes
 bash -lc ""
 :: Install and update necessary packages
 bash -lc "for i in {1..3}; do update-core && break || sleep 15; done"
-bash -lc "for i in {1..3}; do pacman --noconfirm -Su mingw-w64-%MSYS2_ARCH%-{gcc,libiconv} automake autoconf make dos2unix && break || sleep 15; done"
+bash -lc "for i in {1..3}; do pacman --noconfirm -Su mingw-w64-%MSYS2_ARCH%-{gcc,make} make && break || sleep 15; done"
 
 bash -lc "make"
 
