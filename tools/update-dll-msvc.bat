63ecbf51b515b696f50c938f62450a17d5db970c
jiangjianshan <jiangjianshan1103@gmail.com>
2020-01-16 17:26:15 +0800

    Update the method to search the location of VC installation path

--- tools/update-dll-msvc.bat
+++ tools/update-dll-msvc.bat
@@ -1,7 +1,6 @@
 @echo off
 rem Update the DLL using MSVC.
-rem If the old DLL is in use, rename it to avoid compilation error.
-rem current support version of Visual C compiler is from 2005 to 2019
+rem current support version of Visual C compiler is 2008 and upper version
 rem
 rem usage: update-dll-msvc
 rem
@@ -18,90 +17,144 @@ rem \     'unix' : 'gmake',
 rem \    },
 rem \ }
 
-setlocal enabledelayedexpansion
-
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    set vimproc_arch=64
-) else (
-    set vimproc_arch=32
-)
+REM --------------------------------------------------------------------------------------------
+REM Set the default value for vimproc_arch and msvc_arch based on processor architecture.
+REM --------------------------------------------------------------------------------------------
+set vimproc_arch=32
+set msvc_arch=x86
+if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" set "vimproc_arch=64" & set "msvc_arch=x86_amd64" & goto Search_VC_Location
+if /i "%PROCESSOR_ARCHITEW6432%"=="AMD64" set "vimproc_arch=64" & set "msvc_arch=x86_amd64" & goto Search_VC_Location
+if /i "%PROCESSOR_ARCHITECTURE%"=="x64"   set "vimproc_arch=64" & set "msvc_arch=x86_amd64" & goto Search_VC_Location
+if /i "%PROCESSOR_ARCHITECTURE%"=="IA64"  set "vimproc_arch=64" & set "msvc_arch=x86_amd64" & goto Search_VC_Location
+if /i "%PROCESSOR_ARCHITEW6432%"=="IA64"  set "vimproc_arch=64" & set "msvc_arch=x86_amd64"
 
 set vimproc_dllname=vimproc_win%vimproc_arch%.dll
 
+:Search_VC_Location
+
+REM --------------------------------------------------------------------------------------------
+REM Determine which registry keys to look at based on architecture type.
+REM --------------------------------------------------------------------------------------------
+if "%vimproc_arch%"=="64" (
+    set VCRegKeyPath=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\SxS\VC7
+    set VSRegKeyPath=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\SxS\VS7
+)    else (
+    set VCRegKeyPath=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VC7
+    set VSRegKeyPath=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VS7
+)
+
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
-    REM  found the lasted version of Visual C compiler
+    REM --------------------------------------------------------------------------------------------
+    REM  found the lasted version of Visual C compiler, which start from 2017
+    REM --------------------------------------------------------------------------------------------
     for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
         set InstallDir=%%i
-
-        if exist "%InstallDir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
-            set /p msvc_ver=<"%InstallDir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
-
-            rem Trim
-            set msvc_ver=!msvc_ver: =!
-        )
-
-        if exist "%InstallDir%\VC\Auxiliary\Build\vcvars%vimproc_arch%.bat" (
-            call "%InstallDir%\VC\Auxiliary\Build\vcvars%vimproc_arch%.bat"
+        if exist "%%i\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
+            set /p msvc_ver=<"%%i\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
         )
     )
-) else if defined VS140COMNTOOLS (
-    REM  Microsoft Visual Studio 2015 installed
-    set msvc_ver=14
-    call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS120COMNTOOLS (
-    REM  Microsoft Visual Studio 2013 installed
-    set msvc_ver=12
-    call "%VS120COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS110COMNTOOLS (
-    REM  Microsoft Visual Studio 2012 installed
-    set msvc_ver=11
-    call "%VS110COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS100COMNTOOLS (
-    REM  Microsoft Visual Studio 2010 installed
-    set msvc_ver=10
-    call "%VS100COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS90COMNTOOLS (
-    REM  Microsoft Visual Studo 2008 installed
-    set msvc_ver=9
-    call "%VS90COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS80COMNTOOLS (
-    REM  Microsoft Visual Studio 2005 installed
-    set msvc_ver=8
-    call "%VS80COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS71COMNTOOLS (
-    REM  Microsoft Visual C++ .NET 2003 installed
-    set msvc_ver=7
-    call "%VS71COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if defined VS70COMNTOOLS (
-    REM  Microsoft Visual C++ .NET 2002 installed
-    set msvc_ver=7
-    call "%VS70COMNTOOLS%\..\..\VC\bin\vcvars%vimproc_arch%.bat"
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\bin\vcvars%vimproc_arch%.bat" (
-    REM  Microsoft Visual C++ 2015 Express installed
-    set msvc_ver=14
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\bin\vcvars%vimproc_arch%.bat"
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\bin\vcvars%vimproc_arch%.bat" (
-    REM  Microsoft Visual C++ 2013 Express installed
-    set msvc_ver=12
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\bin\vcvars%vimproc_arch%.bat"
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\bin\vcvars%vimproc_arch%.bat" (
-    REM  Microsoft Visual C++ 2012 Express installed
-    set msvc_ver=11
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\bin\vcvars%vimproc_arch%.bat"
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\bin\vcvars%vimproc_arch%.bat" (
-    REM  Microsoft Visual C++ 2010 Express installed
-    set msvc_ver=10
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\bin\vcvars%vimproc_arch%.bat"
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars%vimproc_arch%.bat" (
-    REM  Microsoft Visual C++ 2008 Express installed
-    set msvc_ver=9
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars%vimproc_arch%.bat"
-) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 8\VC\bin\vcvars%vimproc_arch%.bat" (
-    REM  Microsoft Visual C++ 2005 Express installed
-    set msvc_ver=8
-    call "%ProgramFiles(x86)%\Microsoft Visual Studio 8\VC\bin\vcvars%vimproc_arch%.bat"
 ) else (
-    echo Warning: Could not find the lower version or environment variables for Visual Studio
+    REM --------------------------------------------------------------------------------------------
+    REM found the version of Visual C compiler which version is lower than 2017
+    REM --------------------------------------------------------------------------------------------
+
+    REM --------------------------------------------------------------------------------------------
+    REM Read the value for VCRoot and VSRoot from the registry.
+    REM Note: The second call to reg will fail if VS is not installed.  These calls to reg are
+    REM checking to see if VS is installed in a custom location.  This behavior is expected in
+    REM this scenario.
+    REM --------------------------------------------------------------------------------------------
+
+    REM --------------------------------------------------------------------------------------------
+    REM Visual Studio 14.0
+    REM --------------------------------------------------------------------------------------------
+    for /f "tokens=2*" %%a in ('reg query "%VCRegKeyPath%" /v 14.0 /reg:32 2^>nul') do (
+        set VCRoot=%%~b
+        set msvc_ver=14
+        goto Start_Build
+    )
+
+    for /f "tokens=2*" %%a in ('reg query "%VSRegKeyPath%" /v 14.0 /reg:32 2^>nul') do (
+        set VSRoot=%%~b
+        set msvc_ver=14
+        goto Start_Build
+    )
+
+    REM --------------------------------------------------------------------------------------------
+    REM Visual Studio 12.0
+    REM --------------------------------------------------------------------------------------------
+    for /f "tokens=2*" %%a in ('reg query "%VCRegKeyPath%" /v 12.0 /reg:32 2^>nul') do (
+        set VCRoot=%%~b
+        set msvc_ver=12
+        goto Start_Build
+    )
+
+    for /f "tokens=2*" %%a in ('reg query "%VSRegKeyPath%" /v 12.0 /reg:32 2^>nul') do (
+        set VSRoot=%%~b
+        set msvc_ver=12
+        goto Start_Build
+    )
+
+    REM --------------------------------------------------------------------------------------------
+    REM Visual Studio 11.0
+    REM --------------------------------------------------------------------------------------------
+    for /f "tokens=2*" %%a in ('reg query "%VCRegKeyPath%" /v 11.0 /reg:32 2^>nul') do (
+        set VCRoot=%%~b
+        set msvc_ver=11
+        goto Start_Build
+    )
+
+    for /f "tokens=2*" %%a in ('reg query "%VSRegKeyPath%" /v 11.0 /reg:32 2^>nul') do (
+        set VSRoot=%%~b
+        set msvc_ver=11
+        goto Start_Build
+    )
+
+    REM --------------------------------------------------------------------------------------------
+    REM Visual Studio 10.0
+    REM --------------------------------------------------------------------------------------------
+    for /f "tokens=2*" %%a in ('reg query "%VCRegKeyPath%" /v 10.0 /reg:32 2^>nul') do (
+        set VCRoot=%%~b
+        set msvc_ver=10
+        goto Start_Build
+    )
+
+    for /f "tokens=2*" %%a in ('reg query "%VSRegKeyPath%" /v 10.0 /reg:32 2^>nul') do (
+        set VSRoot=%%~b
+        set msvc_ver=10
+        goto Start_Build
+    )
+
+    REM --------------------------------------------------------------------------------------------
+    REM Visual Studio 9.0
+    REM --------------------------------------------------------------------------------------------
+    for /f "tokens=2*" %%a in ('reg query "%VCRegKeyPath%" /v 9.0 /reg:32 2^>nul') do (
+        set VCRoot=%%~b
+        set msvc_ver=9
+        goto Start_Build
+    )
+
+    for /f "tokens=2*" %%a in ('reg query "%VSRegKeyPath%" /v 9.0 /reg:32 2^>nul') do (
+        set VSRoot=%%~b
+        set msvc_ver=9
+        goto Start_Build
+    )
+)
+
+:Start_Build
+
+if exist "%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "%InstallDir%"\VC\Auxiliary\Build\vcvarsall.bat %msvc_arch%
+) else if exist "%VSRoot%vcvarsall.bat" (
+    call "%VSRoot%vcvarsall.bat %msvc_arch%"
+) else if exist "%VCRoot%vcvarsall.bat" (
+    call "%VCRoot%vcvarsall.bat %msvc_arch%"
+) else if exist "%VSRoot%bin\vcvars%vimproc_arch%.bat" (
+    call "%VSRoot%bin\vcvars%vimproc_arch%.bat"
+) else if exist "%VCRoot%bin\vcvars%vimproc_arch%.bat" (
+    call "%VCRoot%bin\vcvars%vimproc_arch%.bat"
+) else (
+    echo "Could not find the lower verison or the installation path of Visual C compiler!"
 )
 
 where nmake >nul 2>&1
@@ -117,18 +170,7 @@ if %msvc_ver% geq 11 (
 )
 
 if errorlevel 1 (
-    rem Build failed.
-
-    rem Try to delete old DLLs.
-    if exist lib\%vimproc_dllname%.old del lib\%vimproc_dllname%.old
-    if exist lib\%vimproc_dllname%     del lib\%vimproc_dllname%
-    rem If the DLL couldn't delete (may be it is in use), rename it.
-    if exist lib\%vimproc_dllname%     ren lib\%vimproc_dllname% %vimproc_dllname%.old
-
-    if %msvc_ver% geq 11 (
-        nmake -f make_msvc.mak nodebug=1 CPU=%PROCESSOR_ARCHITECTURE% "SDK_INCLUDE_DIR=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include"
-    ) else (
-        nmake -f make_msvc.mak nodebug=1 CPU=%PROCESSOR_ARCHITECTURE% 
-    )
+    rem Build failed, then clean the generated files.
+    nmake -f make_msvc.mak clean
 )
 
